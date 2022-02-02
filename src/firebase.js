002bce628aff0f504092d714dfed458ddbf56640
ssseok <87220944+ssseok@users.noreply.github.com>
2022-02-03 00:04:11 +0900

    seok5

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyArsKQ_Ay1TanT15n8S4T4lJ83oKkHfBkI",
-  authDomain: "sseok-4830d.firebaseapp.com",
-  projectId: "sseok-4830d",
-  storageBucket: "sseok-4830d.appspot.com",
-  messagingSenderId: "146770204472",
-  appId: "1:146770204472:web:337c7290ffb7aa5d6dd3cd",
-  measurementId: "G-6K58T1TC6Q",
+  apiKey: "AIzaSyC235jUMITImSQQ59kBqN_8dtHsoRmRDM4",
+  authDomain: "seokbobo-2c7fa.firebaseapp.com",
+  projectId: "seokbobo-2c7fa",
+  storageBucket: "seokbobo-2c7fa.appspot.com",
+  messagingSenderId: "445305738350",
+  appId: "1:445305738350:web:a1b9ae42cda99e8216027f",
+  measurementId: "G-8VFTMCSKWQ",
 };
 
 initializeApp(firebaseConfig);
